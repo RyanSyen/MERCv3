e18208a25b6ecd8468b8bec8f3ad1ad39c53df95
RyanSyen <ryanwong710200@gmail.com>
2022-08-06 04:53:54 +0800

    successfully uploaded doc to realtime database

--- src/app/component/test/confirmation/confirmation.component.ts
+++ src/app/component/test/confirmation/confirmation.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FileUploadService } from 'src/app/service/file-upload.service';
+import { FileUpload } from 'src/app/models/file-upload.model';
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationComponent implements OnInit {
 
-  constructor() { }
-
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+  constructor(private uploadService: FileUploadService) { }
   ngOnInit(): void {
+  }
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+  upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+      if (file) {
+        this.currentFileUpload = new FileUpload(file);
+        this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(
+          percentage => {
+            this.percentage = Math.round(percentage ? percentage : 0);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    }
   }
 
 }
--- src/app/component/test/payment/payment.component.ts
+++ src/app/component/test/payment/payment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FileUploadService } from 'src/app/service/file-upload.service';
+import { FileUpload } from 'src/app/models/file-upload.model';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
-
+  @Input() fileUpload!: FileUpload;
+  constructor(private uploadService: FileUploadService) { }
   ngOnInit(): void {
+  }
+  deleteFileUpload(fileUpload: FileUpload): void {
+    this.uploadService.deleteFile(fileUpload);
   }
 
 }
